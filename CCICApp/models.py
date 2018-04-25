d8c2202ce42e496079ee0132ccace6135787c557
[wuyue2965] <[wuyue2965@gmail.com]>
2018-04-25 15:08:41 +0800

    model添加知乎部分

--- CCICApp/models.py
+++ CCICApp/models.py
@@ -14,3 +14,13 @@ class vvebo(models.Model):
     dianzan = models.IntegerField(default=0)
     device = models.TextField(max_length=1000, default="")
     url = models.TextField(max_length=1000, default="")
+
+class Zhihu(models.Model):
+    keyword = models.TextField(max_length=100, default="")
+    question_id = models.CharField(max_length=20, default="")
+    question_name = models.TextField(max_length=100, default="")
+    answer_id = models.CharField(max_length=20, default="")
+    comment = models.TextField(max_length=30000, default="")
+    time = models.CharField(max_length=20, default="")
+    voteup_count = models.IntegerField(default=0)
+    user_name = models.CharField(max_length=20, default="")
